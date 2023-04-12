14273ac75bc68b5c709129282fb48ff52d51f070
URIAN VIERA <urian1213viera@gmail.com>
2023-04-11 21:41:27 -0500

    Update formLogin.php

--- formLogin.php
+++ formLogin.php
@@ -82,9 +82,7 @@ if (isset($_SESSION['emailUser']) == "") { ?>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 		<script src="assets/js/script.js"></script>
-		<script>
-			toastr.info('Hola, llena todos los campos para crear tu cuenta.');
-		</script>
+		<?php include("msjs.php"); ?>
 	</body>
 
 	</html>
